62a07310d743f87c5867beca39efe82d61ecae55
Frederik Tilkin <frederik.tilkin@vsko.be>
2023-02-10 00:56:20 +0100

    Simplified the itr8FromStdIn operator so it will return strings instead of single characters, and leave it up to the user to pipe that into stringToChar() or lineByLine() if needed.

--- src/peer/stream.ts
+++ src/peer/stream.ts
@@ -9,9 +9,18 @@ import { TPipeable } from '../types';
 /**
  * Transforms a readable stream into an async itr8.
  *
+ * Each chunk of data wil be passed as a string if a default encoding has been specified
+ * for the stream using the readable.setEncoding() method; otherwise the data will be passed
+ * as a Buffer!
+ *
  * @example
  * ```typescript
- * itr8FromStream(fs.createReadStream("D://data.txt"));
+ * // each next() call will return a Buffer
+ * itr8FromStream(fs.createReadStream("./myfile.bin"));
+ * // if you have a txt file you want to process line by line:
+ * const readableStream = fs.createReadStream("D://mytextfile.txt");
+ * readableStream.setEncoding('utf8');
+ * itr8FromStream(readableStream).pipe(lineByLine());
  * ```
  *
  * Did you know that [all readable streams are Iterables](https://2ality.com/2018/04/async-iter-nodejs.html#reading-asynchronously-via-async-iteration)?
@@ -70,23 +79,27 @@ const itr8FromStream = (stream:Stream.Readable):TPipeable & AsyncIterableIterato
 }
 
 /**
- * This will produce an AsyncIterableIterator where each value is one character
+ * This will produce an AsyncIterableIterator where each value is a string
  * from the stdin stream.
  *
- * Really useful combined with the lineByLine() operator to process large files.
+ * Really useful combined with the lineByLine() operators to process large files one line at a time.
+ * If you want to get every single character, use the stringToChar() operator.
  *
  * (If you want the raw bytes, simply use `itr8FromStream(process.stdin)`)
  *
- * @returns
+ * @returns an iterator of strings (chunks of data read from stdin)
  *
  * @category peer/stream
  */
-const itr8FromStdin:()=>TPipeable & AsyncIterableIterator<string> = () => itr8FromStream(process.stdin)
-        .pipe(
-          map(x => x.toString()),
-          stringToChar(),
-        );
-
+const itr8FromStdin:()=>TPipeable & AsyncIterableIterator<string> = () => {
+  process.stdin.setEncoding('utf8');
+  return itr8FromStream(process.stdin);
+    // OBSOLETE BECAUSE OF the setEnconding
+    // .pipe(
+    //   map(x => x.toString()),
+    //   // stringToChar(),
+    // );
+}
 /**
  * Creates a readable (object-mode) stream from a (sync or async) iterator.
  *
